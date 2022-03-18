91fe41a8b9106400d0b25900f638e4377fe88bac
kalends24 <2431459249@qq.com>
2022-03-19 06:16:23 +0800

    dev

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import  router from '@/router'
 import TypeNav from '@/pages/Home/TypeNav'
 //第一个参数:全局组件的名字，第二个参数：哪一个组件
 Vue.component(TypeNav.name,TypeNav)
-console.log(this);
 Vue.config.productionTip = false
 new Vue({
   router,
